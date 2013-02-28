1b829d6c8fcfc253182d1caf7564c43cd1a82e2f
ssherar <s.sherar@live.com>
2013-02-28 12:31:00 +0000

    adding remove prototype function

--- game.js
+++ game.js
@@ -23,13 +23,6 @@ $(function() {
 
 	function update() {
 		canvas.clearRect(0,0,CANVAS_WIDTH, CANVAS_HEIGHT);
-		//player.x += 10 * inverse;
-		//player.y += 10 * inverse;
-		//if(inverse == 1 && player.x > (CANVAS_WIDTH - margin)) {
-		//	inverse = -1;
-		//} else if(inverse == -1 && player.x < (0 + margin)) {
-		//	inverse = 1;
-		//}
 	}
 
 	function draw() {
@@ -49,22 +42,47 @@ $(function() {
 			 canvas.fillRect(this.x, this.y, this.width, this.height);
 		 },
 		 moveLeft: function() {
-			 this.x = ((this.x - 10) < 0) ? 0 : this.x - 10;
+			 this.x = clamp(this.x - 10, 0, CANVAS_WIDTH);
 		 },
 		 moveRight: function() {
-			 this.x = ((this.x + 10) > CANVAS_WIDTH-this.width) ? CANVAS_WIDTH - this.width : this.x + 10;
+			 this.x = clamp(this.x + 10, 0, CANVAS_WIDTH - this.width);
+		 },
+		 moveUp: function() {
+			this.y = clamp(this.y - 10, 0, CANVAS_HEIGHT);	 
+		 }, moveDown: function() {
+			 this.y = clamp(this.y + 10, 0, CANVAS_HEIGHT - this.height);
+		 }, fire: function() {
+			 console.log("PEW PEW MOTHERFUCKER!");
 		 }
 	};
 
+	var pressed_keys = [];
+	var valid_keys = [32, 37, 38, 39, 40];
+
 	$(window).keydown(function(event) {
-		switch(event.which) {
-			case 37:
-				player.moveLeft();
-				break;
-			case 39:
-				player.moveRight();
-				break;
+		if($.inArray(event.which, valid_keys) > -1) {
+			if($.inArray(event.which, pressed_keys) == -1) {
+				pressed_keys.push(event.which);
+			}
 		}
 	});
+
+	$(window).keyup(function(event) {
+		if($.inArray(event.which, pressed_keys) > -1) {
+			pressed_keys.remove(event.which);
+		}
+	});
+
+	var clamp = function(x, min, max) {
+		return (x < min ? min : (x > max ? max : x ));
+	}
+
+	Array.prototype.remove = function(ele) {
+		var where;
+		if((where = this.indexOf(ele)) > -1) {
+			this.splice(where, 1);
+		}
+		return this;
+	}
 
 });
